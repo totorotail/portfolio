7203da872875a5094d59dd0d5e1813df6594dce6
totorotail <jminkyun@gmail.com>
2025-02-24 13:13:39 +0900

    refactor: navbar 컴포넌트로

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,12 @@
 <script setup>
+  import Navbar from './components/Navbar.vue';
 </script>
 
 <template>
-  <nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top" data-bs-theme="dark">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">Sample</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav ms-auto">
-            <li class="nav-item">
-            <router-link 
-              class="nav-link active" 
-              aria-current="page" 
-              to="/">Home
-            </router-link>
-          </li>
-          <li class="nav-item">
-            <router-link 
-              class="nav-link" 
-              to="/portfolio"
-            >Portfolio</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link 
-              class="nav-link" 
-              to="/about"
-            >About</router-link>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-
+  <Navbar />
   <router-view />
 
 </template>
 
 <style scoped lang="scss">
-  .navbar {
-    .nav-item { margin-left: 2em; }
-  }
 </style>
